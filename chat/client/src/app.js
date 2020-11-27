66f261c60add77be130d45abda1f7d1b4acadb36
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-27 14:15:04 -0800

    Create app.js

---
+++ chat/client/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Join from './components/join/join';
+import Chat from './components/chat/chat';
+
+const App = () => (
+    <BrowserRouter>
+        <Route path="/" exact component = {Join}/>
+        <Route path="/chat" component = {Chat}/>
+    </BrowserRouter>
+);
+
+export default App;
